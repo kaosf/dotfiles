9fe780600b8e8fdc37168f6fd4414f4528f71854
ka <ka.kaosf@gmail.com>
2013-01-21 17:59:44 +0900

    add .tmux.conf

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
-ln -s $PWD/.zshrc    $HOME/.zshrc
-ln -s $PWD/.screenrc $HOME/.screenrc
-ln -s $PWD/.vimrc    $HOME/.vimrc
+ln -s $PWD/.zshrc     $HOME/.zshrc
+ln -s $PWD/.screenrc  $HOME/.screenrc
+ln -s $PWD/.vimrc     $HOME/.vimrc
+ln -s $PWD/.tmux.conf $HOME/.tmux.conf
