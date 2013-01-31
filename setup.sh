c2f5c3d460755bb215d98f4e98302af5997926a2
ka <ka.kaosf@gmail.com>
2013-01-31 22:28:35 +0900

    add a setup script for Emacs

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ ln -s $PWD/.zshrc     $HOME/.zshrc
 ln -s $PWD/.screenrc  $HOME/.screenrc
 ln -s $PWD/.vimrc     $HOME/.vimrc
 ln -s $PWD/.tmux.conf $HOME/.tmux.conf
+ln -s $PWD/.emacs     $HOME/.emacs
