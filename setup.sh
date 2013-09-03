40dfab7589613cf1d24b1d46b5aa72c53d8425a1
ka <ka.kaosf@gmail.com>
2013-09-04 01:16:28 +0900

    modify setup.sh for .xmodmap and .xmodmap-for-hhkb

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 
-ln -s $PWD/.zshrc     $HOME/.zshrc
-ln -s $PWD/.vimrc     $HOME/.vimrc
-ln -s $PWD/.tmux.conf $HOME/.tmux.conf
-ln -s $PWD/.emacs     $HOME/.emacs
+ln -s $PWD/.zshrc            $HOME/.zshrc
+ln -s $PWD/.vimrc            $HOME/.vimrc
+ln -s $PWD/.tmux.conf        $HOME/.tmux.conf
+ln -s $PWD/.emacs            $HOME/.emacs
+ln -s $PWD/.xmodmap          $HOME/.xmodmap
+ln -s $PWD/.xmodmap-for-hhkb $HOME/.xmodmap-for-hhkb
