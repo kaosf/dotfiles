ddd8d2e674721415bfc0ed8337106921bb3552dd
ka <ka.kaosf@gmail.com>
2013-09-04 16:08:48 +0900

    don't create symlink of .xmodmap-for-hhkb

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 #! /bin/sh
 
-ln -s $PWD/.zshrc            $HOME/.zshrc
-ln -s $PWD/.vimrc            $HOME/.vimrc
-ln -s $PWD/.tmux.conf        $HOME/.tmux.conf
-ln -s $PWD/.emacs            $HOME/.emacs
-ln -s $PWD/.xmodmap          $HOME/.xmodmap
-ln -s $PWD/.xmodmap-for-hhkb $HOME/.xmodmap-for-hhkb
+ln -s $PWD/.zshrc     $HOME/.zshrc
+ln -s $PWD/.vimrc     $HOME/.vimrc
+ln -s $PWD/.tmux.conf $HOME/.tmux.conf
+ln -s $PWD/.emacs     $HOME/.emacs
+ln -s $PWD/.xmodmap   $HOME/.xmodmap
