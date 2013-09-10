f7c21bf03d87ad9dc9a0373c612b7ff1fb0550cb
ka <ka.kaosf@gmail.com>
2013-09-11 00:31:23 +0900

    install clojure-mode for Emacs with package.el

--- .emacs.d_init.el
+++ .emacs.d_init.el
@@ -4,3 +4,18 @@
 (setq visible-bell t)
 
 (setq make-backup-files nil)
+
+;; for package.el
+;; ref.
+;;   http://d.hatena.ne.jp/syohex/20130624/1372082597
+;;   https://github.com/clojure-emacs/clojure-mode
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+;; I executed following commands for install clojure-mode
+;; 2013-09-11
+;; M-x eval-buffer
+;; M-x package-refresh-contents
+;; M-x package-install [RET] clojure-mode [RET]
