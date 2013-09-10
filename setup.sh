29099029eff805c032c858ba05c9789b40f42f94
ka <ka.kaosf@gmail.com>
2013-09-10 11:42:25 +0900

    use .emacs.d/init.el instead of .emacs

--- setup.sh
+++ setup.sh
@@ -3,4 +3,8 @@
 ln -s $PWD/.zshrc     $HOME/.zshrc
 ln -s $PWD/.vimrc     $HOME/.vimrc
 ln -s $PWD/.tmux.conf $HOME/.tmux.conf
-ln -s $PWD/.emacs     $HOME/.emacs
+if [ ! -d $HOME/.emacs.d ]
+then
+  mkdir $HOME/.emacs.d
+fi
+ln -s $PWD/.emacs.d_init.el $HOME/.emacs.d/init.el
