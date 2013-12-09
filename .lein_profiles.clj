023b94b2fd00702ba48e1ba3b689ec02682180d4
ka <ka.kaosf@gmail.com>
2013-12-09 21:09:04 +0900

    modify .lein_profiles.clj

--- .lein_profiles.clj
+++ .lein_profiles.clj
@@ -1,4 +1,6 @@
-{:user {:plugins [
-  [colorize "0.1.1"]
-  [lein-exec "0.3.1"]
-  [lein-droid/lein-droid "0.2.0"]]}}
+{:user
+ {:plugins [[colorize "0.1.1"]
+            [lein-exec "0.3.1"]
+            [lein-droid/lein-droid "0.2.0"]]
+  ;:android {:sdk-path "/path/to/android-sdk/"}
+}}
