9d0b3c2188a8b4a5568e89df0e5552921c788814
ka <ka.kaosf@gmail.com>
2014-01-23 11:29:16 +0900

    use org.clojure/math.numeric-tower 0.0.4

--- .lein_profiles.clj
+++ .lein_profiles.clj
@@ -1,6 +1,7 @@
 {:user
  {:plugins [[colorize "0.1.1"]
             [lein-exec "0.3.1"]
-            [lein-droid/lein-droid "0.2.0"]]
+            [lein-droid/lein-droid "0.2.0"]
+            [org.clojure/math.numeric-tower "0.0.4"]]
   ;:android {:sdk-path "/path/to/android-sdk/"}
 }}
