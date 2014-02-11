cdb7ce470e4cf00b001a4b0c0fd9cd0e607b841b
ka <ka.kaosf@gmail.com>
2014-02-12 02:41:48 +0900

    Revert "use org.clojure/math.numeric-tower 0.0.4"
    This reverts commit 9d0b3c2188a8b4a5568e89df0e5552921c788814.

--- .lein_profiles.clj
+++ .lein_profiles.clj
@@ -1,7 +1,6 @@
 {:user
  {:plugins [[colorize "0.1.1"]
             [lein-exec "0.3.1"]
-            [lein-droid/lein-droid "0.2.0"]
-            [org.clojure/math.numeric-tower "0.0.4"]]
+            [lein-droid/lein-droid "0.2.0"]]
   ;:android {:sdk-path "/path/to/android-sdk/"}
 }}
