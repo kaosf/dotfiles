fa998491d032a526be319c8f873a3893aa9920fe
ka <ka.kaosf@gmail.com>
2014-03-01 11:53:02 +0900

    use lein-typed plugin version 0.3.1

--- .lein_profiles.clj
+++ .lein_profiles.clj
@@ -1,6 +1,7 @@
 {:user
  {:plugins [[colorize "0.1.1"]
             [lein-exec "0.3.1"]
-            [lein-droid/lein-droid "0.2.0"]]
+            [lein-droid/lein-droid "0.2.0"]
+            [lein-typed "0.3.1"]]
   ;:android {:sdk-path "/path/to/android-sdk/"}
 }}
