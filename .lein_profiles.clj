b160ad7bc897ccf1d93960ac80ae1c6149d75fd1
ka <ka.kaosf@gmail.com>
2015-02-02 13:06:43 +0900

    Modify .lein_profiles.clj
    Remove colorize.
    Fix indent.

--- .lein_profiles.clj
+++ .lein_profiles.clj
@@ -1,7 +1,10 @@
-{:user
- {:plugins [[colorize "0.1.1"]
-            [lein-exec "0.3.1"]
-            [lein-droid/lein-droid "0.2.0"]
-            [lein-typed "0.3.1"]]
-  ;:android {:sdk-path "/path/to/android-sdk/"}
-}}
+{
+  :user {
+    :plugins [
+      [lein-exec "0.3.1"]
+      [lein-droid/lein-droid "0.2.0"]
+      [lein-typed "0.3.1"]
+    ]
+    ;:android {:sdk-path "/path/to/android-sdk/"}
+  }
+}
