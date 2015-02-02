d0a88a4b66de105ccd5d5d6ee850734f24491f5d
ka <ka.kaosf@gmail.com>
2015-02-02 13:08:36 +0900

    Add REPL prompt modification (comment out it now)

--- .lein_profiles.clj
+++ .lein_profiles.clj
@@ -6,5 +6,8 @@
       [lein-typed "0.3.1"]
     ]
     ;:android {:sdk-path "/path/to/android-sdk/"}
+
+    ; ref. http://stackoverflow.com/questions/11598066/can-i-add-a-newline-to-the-clojure-repl-prompt
+    ;:repl-options {:prompt #(str %1 "=>\n")}
   }
 }
