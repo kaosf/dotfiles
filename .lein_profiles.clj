ad927059b75bfac56dc68c7da3bda0d289be4e48
ka <ka.kaosf@gmail.com>
2015-03-11 21:26:49 +0900

    Use "lein-autoexpect" plugin version 1.4.2
    ref.
      http://dev.classmethod.jp/client-side/language-client-side/lein-autoexpect/
      https://github.com/jakemcc/lein-autoexpect

--- .lein_profiles.clj
+++ .lein_profiles.clj
@@ -4,6 +4,10 @@
       [lein-exec "0.3.1"]
       [lein-droid/lein-droid "0.2.0"]
       [lein-typed "0.3.1"]
+      ; ref.
+      ;   http://dev.classmethod.jp/client-side/language-client-side/lein-autoexpect/
+      ;   https://github.com/jakemcc/lein-autoexpect
+      [lein-autoexpect "1.4.2"]
     ]
     ;:android {:sdk-path "/path/to/android-sdk/"}
 
