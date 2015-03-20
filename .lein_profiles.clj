b7f407dbe6c300713a189fb8a4c467ac7eadeace
ka <ka.kaosf@gmail.com>
2015-03-21 03:15:31 +0900

    Add empty lines features separation

--- .lein_profiles.clj
+++ .lein_profiles.clj
@@ -2,16 +2,22 @@
   :user {
     :plugins [
       [lein-exec "0.3.1"]
+
       [lein-droid/lein-droid "0.2.0"]
+
       [lein-typed "0.3.1"]
+
       ; ref. https://github.com/weavejester/lein-auto
       ;[lein-auto "0.1.1"]
+
       ; ref. https://github.com/jakepearson/quickie
       ;[quickie "0.3.6"]
+
       ; ref.
       ;   http://dev.classmethod.jp/client-side/language-client-side/lein-autoexpect/
       ;   https://github.com/jakemcc/lein-autoexpect
       ;[lein-autoexpect "1.4.2"]
+
       ; For tmux status bar notification
       ; ref. http://www.stathis.co.uk/lein-test-refresh-notification-workflow/
       [com.jakemccrary/lein-test-refresh "0.5.0"]
