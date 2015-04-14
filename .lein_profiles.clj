6850d4db62f0de1adb1303bf998517aa7597f3f9
ka <ka.kaosf@gmail.com>
2015-04-15 00:27:56 +0900

    Add venantius/ultra to .lein/profiles.clj for colorizing on REPL
    ref. https://github.com/venantius/ultra

--- .lein_profiles.clj
+++ .lein_profiles.clj
@@ -22,6 +22,12 @@
       ; Now, I can do automated test by running "lein test-refresh" command.
       ; ref. http://www.stathis.co.uk/lein-test-refresh-notification-workflow/
       [com.jakemccrary/lein-test-refresh "0.5.0"]
+
+      ; For colorizing on REPL
+      ; ref.
+      ;   https://github.com/venantius/ultra
+      ;   https://github.com/venantius/ultra#installation
+      [venantius/ultra "0.3.3"]
     ]
     ;:android {:sdk-path "/path/to/android-sdk/"}
 
@@ -32,5 +38,8 @@
 
     ; ref. http://stackoverflow.com/questions/11598066/can-i-add-a-newline-to-the-clojure-repl-prompt
     ;:repl-options {:prompt #(str %1 "=>\n")}
+
+    ; ref. https://github.com/venantius/ultra#installation
+    :ultra {:color-scheme :solarized_dark}
   }
 }
