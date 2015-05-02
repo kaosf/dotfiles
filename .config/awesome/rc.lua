d510b7a6b383240b772e2ff868efb1414246b67d
ka <ka.kaosf@gmail.com>
2015-05-02 22:14:34 +0900

    Change terminal to gnome-terminal

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -44,7 +44,7 @@ end
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "x-terminal-emulator"
+terminal = "gnome-terminal"
 editor = os.getenv("EDITOR") or "editor"
 editor_cmd = terminal .. " -e " .. editor
 
