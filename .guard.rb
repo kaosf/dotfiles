e9ecd5ed8f393c55fb1e4ccb4de3d87109139ab0
ka <ka.kaosf@gmail.com>
2015-09-18 22:25:22 +0900

    Add .guard.rb

---
+++ .guard.rb
@@ -0,0 +1,11 @@
+notification :tmux,
+  display_message: true,
+  timeout: 5,
+  default_message_format: '%s >> %s',
+  line_separator: ' > ',
+  color_location: 'status-left-bg',
+  default_message_color: 'black',
+  success: 'colour150',
+  failure: 'colour174',
+  pending: 'colour179',
+  display_on_all_clients: true
