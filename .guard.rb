ed91340a3ace712102e3b5d7def38e20453ab85f
ka <ka.kaosf@gmail.com>
2015-09-19 21:04:58 +0900

    Switch Guard notification wheather on Tmux or not

--- .guard.rb
+++ .guard.rb
@@ -1,11 +1,15 @@
-notification :tmux,
-  display_message: true,
-  timeout: 5,
-  default_message_format: '%s >> %s',
-  line_separator: ' > ',
-  color_location: 'status-left-bg',
-  default_message_color: 'black',
-  success: 'colour150',
-  failure: 'colour174',
-  pending: 'colour179',
-  display_on_all_clients: true
+if ENV['TMUX']
+  notification :tmux,
+    display_message: true,
+    timeout: 5,
+    default_message_format: '%s >> %s',
+    line_separator: ' > ',
+    color_location: 'status-left-bg',
+    default_message_color: 'black',
+    success: 'colour150',
+    failure: 'colour174',
+    pending: 'colour179',
+    display_on_all_clients: true
+else
+  notification :off
+end
