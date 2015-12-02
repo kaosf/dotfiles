50f95d5dbee2914658f80e5a983f7779f5cd6f44
ka <ka.kaosf@gmail.com>
2015-12-03 00:18:13 +0900

    Create bashrc-setup.sh

---
+++ bashrc-setup.sh
@@ -0,0 +1,17 @@
+if [ -L $HOME/.bashrc ]; then
+  exit 0
+fi
+
+if cat $HOME/.bashrc | grep 'export PROMPT_COMMAND=__prompt_command'; then
+  exit 0
+fi
+
+cat <<EOS >> $HOME/.bashrc
+export PROMPT_COMMAND=__prompt_command
+function __prompt_command() {
+  local EXIT="\$?"
+  PS1='[\\u@\\h \\W'
+  if [ \$EXIT != 0 ]; then PS1+=" \$EXIT"; fi
+  PS1+='] \\\$ '
+}
+EOS
