fc7ca0dfad1bf0e5a0f48adf518ef9177beb51c2
ka <ka.kaosf@gmail.com>
2015-12-13 22:23:27 +0900

    Fix layouts order

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -62,7 +62,7 @@ modkey = "Mod4"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 local layouts =
 {
-    awful.layout.suit.floating,
+    -- awful.layout.suit.floating,
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
@@ -74,6 +74,8 @@ local layouts =
     awful.layout.suit.max,
     awful.layout.suit.max.fullscreen,
     awful.layout.suit.magnifier
+    ,
+    awful.layout.suit.floating
 }
 -- }}}
 
