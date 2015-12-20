5067be39bb09a101d51abbdcd099c2a877fb1484
ka <ka.kaosf@gmail.com>
2015-12-20 23:45:19 +0900

    Enable Guard tmux notification only on SSH

--- .guard.rb
+++ .guard.rb
@@ -1,15 +1,17 @@
-if ENV['TMUX']
-  notification :tmux,
-    display_message: true,
-    timeout: 5,
-    default_message_format: '%s >> %s',
-    line_separator: ' > ',
-    color_location: 'status-left-bg',
-    default_message_color: 'black',
-    success: 'colour150',
-    failure: 'colour174',
-    pending: 'colour179',
-    display_on_all_clients: true
-else
-  notification :off
+if ENV['SSH_CLIENT']
+  if ENV['TMUX']
+    notification :tmux,
+      display_message: true,
+      timeout: 5,
+      default_message_format: '%s >> %s',
+      line_separator: ' > ',
+      color_location: 'status-left-bg',
+      default_message_color: 'black',
+      success: 'colour150',
+      failure: 'colour174',
+      pending: 'colour179',
+      display_on_all_clients: true
+  else
+    notification :off
+  end
 end
